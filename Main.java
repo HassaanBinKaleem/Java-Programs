85134a8c357ac3e883a539cd01f56da4dc0694ca
Hassaan-Bin-Kaleem <105790628+HassaanBinKaleem@users.noreply.github.com>
2022-06-14 18:35:23 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+class  Main
+{
+    static int myMethod(int x,int y)
+    {
+        return y+x;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.print(myMethod(3,6));
+    }
+
+}
